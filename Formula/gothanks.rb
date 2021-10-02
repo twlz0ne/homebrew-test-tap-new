a6176e67137314fb47d587ac306a11e0197ec391
twlz0ne <2653486+twlz0ne@users.noreply.github.com>
2021-10-02 17:57:28 +0000

    gothanks: add 0.4.0 bottle.

--- Formula/gothanks.rb
+++ Formula/gothanks.rb
@@ -5,6 +5,12 @@ class Gothanks < Formula
   sha256 "300b705751a43f6ae25df57d6c9a5b0c859e92d61fa83cf894329ea9662525f2"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/twlz0ne/homebrew-test-tap-new/releases/download/gothanks-0.4.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "774a80c6e98782592408fb98d1620c8f0ab977da3d5d111fae01dd510c2d55cf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "198677980d4a2ec409ff4d406ab26e9c7bbd3273fc95214b484cdb33a2ffb143"
+  end
+
   depends_on "go" => :build
 
   def install
